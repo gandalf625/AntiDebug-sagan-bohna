76808cadd1085d07e4ae0bc5e1dc2e1a3253a206
harbingerr <milanbohna@gmail.com>
2020-11-29 01:17:00 +0100

    TLS Callback

--- AllCombined/AllCombined.cpp
+++ AllCombined/AllCombined.cpp
@@ -4,7 +4,58 @@
 #include <iostream>
 #include <windows.h>
 
+int isDebbuged;
 
+//TLS CALLBACK With IsDebuggedPresent
+void NTAPI TLSEntry(PVOID DllHandle, DWORD dwReason, PVOID)
+{
+    __asm
+    {
+        mov eax, dword ptr fs : [00000030h]
+        movzx eax, byte ptr[eax + 2]
+        mov isDebbuged, eax
+    }
+
+    if (isDebbuged)
+    {
+        int msgboxID = MessageBox(
+            NULL,
+            (LPCWSTR)L"SKUS ZNOVU",
+            (LPCWSTR)L"NEVIES HACKOVAT",
+            MB_ICONERROR | MB_OK
+        );
+        ExitProcess(0);
+    }
+}
+
+#ifdef _WIN64
+#pragma comment (linker, "/INCLUDE:_tls_used")
+#pragma comment (linker, "/INCLUDE:tls_callback")
+#else
+#pragma comment (linker, "/INCLUDE:__tls_used")
+#pragma comment (linker, "/INCLUDE:_tls_callback")
+#endif
+
+#ifdef _WIN64
+#pragma const_seg(".CRT$XLF")
+EXTERN_C const
+#else
+#pragma data_seg(".CRT$XLF")
+EXTERN_C
+#endif
+PIMAGE_TLS_CALLBACK tls_callback = TLSEntry;
+#ifdef _WIN64
+#pragma const_seg()
+#else
+#pragma data_seg()
+#endif //_WIN64
+
+DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
+{
+    ExitThread(0);
+}
+
+//Hiding Console
 void Stealth()
 {
     HWND Stealth;
@@ -15,7 +66,7 @@ void Stealth()
 
 bool IsDebuggerPresentNtQuery()
 {
-    typedef unsigned long(__stdcall* pfnNtQueryInformationProcess)(IN  HANDLE,IN  unsigned int, OUT PVOID, IN ULONG, OUT PULONG);
+    typedef unsigned long(__stdcall* pfnNtQueryInformationProcess)(IN  HANDLE, IN  unsigned int, OUT PVOID, IN ULONG, OUT PULONG);
 
     // ProcessDebugPort
     const int ProcessDbgPort = 7;
@@ -32,10 +83,10 @@ bool IsDebuggerPresentNtQuery()
     NtQueryInfoProcess = (pfnNtQueryInformationProcess)GetProcAddress(hNtDll, "NtQueryInformationProcess");
 
     // Time to finally make the call
-    Ret = NtQueryInfoProcess(GetCurrentProcess(), ProcessDbgPort,&IsRemotePresent, sizeof(unsigned long), NULL);
+    Ret = NtQueryInfoProcess(GetCurrentProcess(), ProcessDbgPort, &IsRemotePresent, sizeof(unsigned long), NULL);
     if (Ret == 0x00000000 && IsRemotePresent != 0)
     {
-        return true; 
+        return true;
     }
 }
 
@@ -66,11 +117,11 @@ bool IsOllyPresnt()
 
     if (handle)
     {
-        return true; 
+        return true;
     }
     else
     {
-        return false; 
+        return false;
     }
 }
 
@@ -80,7 +131,7 @@ bool IsDebuggerPresentSEH_DivZero()
     __try
     {
         int i = 0, j = 1;
-        j /= i; 
+        j /= i;
         printf("%d", j);
     }
     __except (EXCEPTION_EXECUTE_HANDLER)
@@ -110,6 +161,7 @@ int main()
 {
     DWORD startTime = GetTickCount();
     Stealth();
+   
 
     if (IsDebuggerPresent())
     {
@@ -137,6 +189,6 @@ int main()
     {
         DisplayError();
     }
-    
+
     system("start notepad.exe");
 }
